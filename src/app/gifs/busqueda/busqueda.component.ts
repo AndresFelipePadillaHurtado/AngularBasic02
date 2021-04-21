eedf06990ce0289d4fb8ff734b7364f4c87b0cff
Andres Felipe Padilla Hurtado <afpadilla84@misena.edu.co>
2021-04-20 22:12:46 -0500

    Consumiendo por primera vez un servicio

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,28 @@
+import { Component, ElementRef, ViewChild} from '@angular/core';
+
+// Servicio del gifs
+import { GifsService } from '../services/gifs.service';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styles: [
+  ]
+})
+export class BusquedaComponent {
+
+  // El ! es para indicar a Angular que el elmento no es nulo
+  // Indicar el tipo HTMLInputElement
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+  
+  // Inyectar el servicio para el historia
+  constructor( private gifsService: GifsService){
+
+  }
+  buscar(){
+    const valor = this.txtBuscar.nativeElement.value;
+    this.gifsService.buscarGifs(valor);
+  }
+
+
+}
---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+// Servicio
+import { GifsService } from '../../gifs/services/gifs.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styles: [
+  ]
+})
+export class SidebarComponent{
+
+  historials:string[] = [];
+
+  constructor(private gifsService: GifsService) { }
+
+  // Metodos // Esto es lo que va a iterar
+  get historial(){
+    return this.gifsService.historial;
+  }
+
+
+}
